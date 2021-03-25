d5a9daed792f012e1f74c2c0690a610e406d5711
Maneesh Yadav <9890015099y@gmail.com>
2021-03-25 20:38:12 +0530

    Update Double Linked-List.py

--- Double Linked-List.py
+++ Double Linked-List.py
@@ -1,55 +1,110 @@
-# Create a Node
-
-class Nodes:
-    def __init__(self, data):
-        self.back = None
-        self.element = data
-        self.next = None
+class node:
+    def __init__(self,data, next=None, prev=None):
+        self.prev = prev
+        self.data = data
+        self.next = next
         
-        
-# Assign a Head element None        
-
-class Doublly:
+class double:
     def __init__(self):
         self.head = None
-        
-        
-# insert the element in empty Linked-List  
     
-    def insert_into_empty(self, data):
+    def empty(self,data):
         if self.head is None:
-            new_node = Nodes(data)
+            new_node = node(data)
             self.head = new_node
-        else:
-            print("list is not empty")
             
             
-#insert The element/data at the starting of head Node       
-            
+    #insert at staring if list       
     def insert_at_starts(self,data):
         if self.head is None:
             new_node = Nodes(data)
             self.head = new_node
             print("node insert")
-            return
-            
+            return    
         new_node = Nodes(data)
         new_node.next = self.head
         self.head.back = new_node
         self.head = new_node  
-        
-
-#interate the element or visiting each Node
-        
-    def triverse(self):
-        if self.head is None:
-            print("list has no element")
-            return
+               
+            
+    #push the element after given data
+    def after_element(self,x, ndata):
         n = self.head
         while n is not None:
-            print(n.element)
-            prev = n.back
-            n = n.next
+            if n.data == x:
+                break
+            pre = n
+            n=n.next     
+        if n is None:
+            print("element is not found")
+        else:
+            new_node = node(ndata)
+            new_node.next = self.head.next
+            self.head.next = new_node
+            new_node.prev = self.head
             
             
+  
+    #push the element before given data
+    def before_element(self, x , data):
+        n = self.head
+        while n is not None:
+            if n.data == x:
+                break
+            pre = n
+            n=n.next   
+        if n is None:
+            print("element is not found")
+        else:
+            new_node = node(data)
+            new_node.next = pre.next
+            pre.next = new_node
+            new_node.prev = pre
+        
+        
+        
+     #visiting each element       
+    def transv(self):
+        if self.head is None:
+            print("List has no Element")
+            return
+        else:
+            n = self.head
+            while n is not None:
+                print(n.data)
+                n = n.next
+                
+    
+    #delete only middle element
+    def delete(self,x):
+        n = self.head
+        if self.head is None:
+            print("List has no Element")
+            return
+        else:
+            while n is not None:
+                if n.data == x:
+                    print("element found !!!!")
+                    break
+                pre = n    
+                n = n.next  
+            if n == None:
+                return
+            pre.next = n.next
             
+            
+    #count the no of element      
+    def count(self):
+        c = 0
+        if self.head is None:
+            print("List has no Element")
+            return
+        else:
+            n = self.head
+            while n is not None:
+                c += 1 
+                n = n.next
+                print(c)
+        
+    
+        
